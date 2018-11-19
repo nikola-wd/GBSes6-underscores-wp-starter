aa231e04840d01eb42153f4921b083b15aab5fda
Nikola Ivanov <nikola_wd@hotmail.com>
2018-11-19 16:02:16 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+	</main><!-- #content -->
+
+	<footer class="footer">
+		<div class="container">
+			Copyright <?php echo date("Y"); ?>
+		</div> <!-- cont -->
+	</footer>
+
+	<?php wp_footer(); ?>	
+
+</body>
+</html>
